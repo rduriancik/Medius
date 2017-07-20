a3bde83f26a6094839435b54fe75030a2fe92123
rduriancik <r.duriancik@gmail.com>
2017-07-20 17:05:22 +0200

    Adjust app style and colors

--- app/src/main/java/com/example/robert/medius/SocialMediaActivity.kt
+++ app/src/main/java/com/example/robert/medius/SocialMediaActivity.kt
@@ -2,11 +2,17 @@ package com.example.robert.medius
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_social_media.*
 
 class SocialMediaActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_social_media)
+
+        switch_facebook.setOnCheckedChangeListener { _, isChecked -> /*TODO if (isChecked)  sign in else  sign of*/ }
+        switch_twitter.setOnCheckedChangeListener { _, isChecked -> /*TODO if (isChecked)  sign in else  sign of*/ }
     }
+
+
 }
