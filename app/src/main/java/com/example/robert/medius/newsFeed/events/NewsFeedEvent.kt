11bff5764c996df58e04c6670078bdda0b70241b
rduriancik <r.duriancik@gmail.com>
2017-08-07 22:45:31 +0200

    Create a NewsFeedEvent class

---
+++ app/src/main/java/com/example/robert/medius/newsFeed/events/NewsFeedEvent.kt
@@ -0,0 +1,8 @@
+package com.example.robert.medius.newsFeed.events
+
+import com.example.robert.medius.newsFeed.entities.News
+
+/**
+ * Created by robert on 7.8.2017.
+ */
+class NewsFeedEvent(var news: List<News>?, var error: String?)
