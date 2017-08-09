d652f9c92f0a80b87d8ef7d0b41aa4b84a165958
rduriancik <r.duriancik@gmail.com>
2017-08-09 14:53:38 +0200

    Implement NewsFeedEvent as a sealed class and create its instances, adjust TwitterApiHelper

--- app/src/main/java/com/example/robert/medius/newsFeed/events/NewsFeedEvent.kt
+++ app/src/main/java/com/example/robert/medius/newsFeed/events/NewsFeedEvent.kt
@@ -1,8 +1,19 @@
 package com.example.robert.medius.newsFeed.events
 
 import com.example.robert.medius.newsFeed.entities.News
+import com.example.robert.medius.newsFeed.types.NewsFeedType
 
 /**
  * Created by robert on 7.8.2017.
  */
-class NewsFeedEvent(var news: List<News>?, var error: String?)
+
+sealed class NewsFeedEvent(var news: List<News>?, var error: String?, val newsFeedType: NewsFeedType)
+
+class InitTimelineEvent(news: List<News>?, error: String?, feedType: NewsFeedType)
+    : NewsFeedEvent(news, error, feedType)
+
+class RefreshEvent(news: List<News>?, error: String?, feedType: NewsFeedType)
+    : NewsFeedEvent(news, error, feedType)
+
+class LoadMoreEvent(news: List<News>?, error: String?, feedType: NewsFeedType)
+    : NewsFeedEvent(news, error, feedType)
--- app/src/main/java/com/example/robert/medius/twitter/TwitterApiHelper.kt
+++ app/src/main/java/com/example/robert/medius/twitter/TwitterApiHelper.kt
@@ -2,7 +2,9 @@ package com.example.robert.medius.twitter
 
 import com.example.robert.medius.libs.base.EventBus
 import com.example.robert.medius.newsFeed.entities.News
-import com.example.robert.medius.newsFeed.events.NewsFeedEvent
+import com.example.robert.medius.newsFeed.events.InitTimelineEvent
+import com.example.robert.medius.newsFeed.events.LoadMoreEvent
+import com.example.robert.medius.newsFeed.events.RefreshEvent
 import com.example.robert.medius.newsFeed.types.NewsFeedType
 import com.twitter.sdk.android.core.Callback
 import com.twitter.sdk.android.core.Result
@@ -16,26 +18,43 @@ import com.twitter.sdk.android.core.services.StatusesService
 
 class TwitterApiHelper(val statusesService: StatusesService, val eventBus: EventBus) {
 
-    fun getTimeline(count: Int = 50, sinceId: Long? = null, maxId: Long? = null) {
+    fun initTimeline() {
+        getTimeline { news, error -> eventBus.post(InitTimelineEvent(news, error, NewsFeedType.TWITTER)) }
+    }
+
+    fun loadMoreTimeline(sinceId: Long? = null,
+                         maxId: Long? = null) {
+        getTimeline(sinceId = sinceId, maxId = maxId) {
+            news, error ->
+            eventBus.post(LoadMoreEvent(news, error, NewsFeedType.TWITTER))
+        }
+    }
+
+    fun refreshTimeline() {
+        getTimeline { news, error -> eventBus.post(RefreshEvent(news, error, NewsFeedType.TWITTER)) }
+    }
+
+    // TODO check kotlin cost - inline
+    private fun getTimeline(count: Int = 50,
+                            sinceId: Long? = null,
+                            maxId: Long? = null,
+                            post: (news: List<News>?, error: String?) -> Unit) {
         statusesService
                 .homeTimeline(count, sinceId, maxId, false, true, false, true)
                 .enqueue(object : Callback<MutableList<Tweet>>() {
                     override fun success(result: Result<MutableList<Tweet>>) {
                         val items = result.data
                         if (items.isNotEmpty()) {
-                            post(mapTweetsToNews(items))
+                            post(mapTweetsToNews(items), null)
                         } else {
-                            post(error = "No items")
+                            post(null, "No items")
                         }
                     }
 
                     override fun failure(exception: TwitterException?) {
-                        post(error = exception.toString())
+                        post(null, exception.toString())
                     }
                 })
     }
 
-    fun post(news: List<News>? = null, error: String? = null) {
-        eventBus.post(NewsFeedEvent(news, error, NewsFeedType.TWITTER))
-    }
 }
