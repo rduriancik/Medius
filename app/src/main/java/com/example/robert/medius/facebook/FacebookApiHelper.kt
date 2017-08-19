7756da6d5f195078a72d5979a9333ef46c7e677a
rduriancik <r.duriancik@gmail.com>
2017-08-19 14:48:12 +0200

    Add GSON dependency. Implement FacebookApiHelpet

--- app/src/main/java/com/example/robert/medius/facebook/FacebookApiHelper.kt
+++ app/src/main/java/com/example/robert/medius/facebook/FacebookApiHelper.kt
@@ -1,41 +1,54 @@
 package com.example.robert.medius.facebook
 
-import android.util.Log
+import com.example.robert.medius.entities.FacebookNews
+import com.example.robert.medius.entities.News
 import com.example.robert.medius.libs.base.EventBus
 import com.example.robert.medius.newsFeed.events.NewsFeedEvent
 import com.example.robert.medius.newsFeed.types.NewsFeedType
 import com.facebook.AccessToken
 import com.facebook.GraphRequest
 import com.facebook.GraphResponse
+import com.google.gson.Gson
 
 /**
  * Created by robert on 17.8.2017.
  */
 
 class FacebookApiHelper(val eventBus: EventBus) {
+    private val gson = Gson()
     private var request: GraphRequest? = GraphRequest.newGraphPathRequest(
             AccessToken.getCurrentAccessToken(),
             "/me/feed",
-            { response ->
-                Log.d("RESPONSE", response.rawResponse)
-                // TODO map
-                request = response.getRequestForPagedResults(GraphResponse.PagingDirection.NEXT)
-            }
+            null
     )
 
     fun loadMoreTimeline() {
-        request?.executeAsync() ?: eventBus.post(NewsFeedEvent.LoadMoreEvent(null, "No more items", NewsFeedType.FACEBOOK))
+        if (request != null) {
+            request!!.callback = createCallback { news, error -> eventBus.post(NewsFeedEvent.LoadMoreEvent(news, error, NewsFeedType.FACEBOOK)) }
+            request!!.executeAsync()
+        } else {
+            eventBus.post(NewsFeedEvent.LoadMoreEvent(null, "No more items", NewsFeedType.FACEBOOK))
+        }
     }
 
     fun refreshTimeline() {
         request = GraphRequest.newGraphPathRequest(
                 AccessToken.getCurrentAccessToken(),
                 "/me/feed",
-                { response ->
-                    request = response.getRequestForPagedResults(GraphResponse.PagingDirection.NEXT)
-                }
+                createCallback { news, error -> eventBus.post(NewsFeedEvent.RefreshEvent(news, error, NewsFeedType.FACEBOOK)) }
         )
-
         request?.executeAsync()
     }
+
+    private fun createCallback(post: (news: List<News>?, error: String?) -> Unit) =
+            GraphRequest.Callback { response ->
+                val result: List<FacebookNews>? = gson.fromJson(response.jsonObject.getJSONArray("data").toString(), Array<FacebookNews>::class.java)?.toList()
+                if (result != null) {
+                    post(mapFacebookNewsToNews(result), null)
+                } else {
+                    post(null, "Error")
+                }
+
+                request = response.getRequestForPagedResults(GraphResponse.PagingDirection.NEXT)
+            }
 }
